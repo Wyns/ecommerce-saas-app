d94e6ab27f51456bb9fb3ae82ee7534a6cbe45fc
jane3wallace <jane3wallace@gmail.com>
2016-01-18 01:55:58 +0000

    fine tuned contact form and added flash messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,19 @@ class ContactsController < ApplicationController
   end
   
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = 'Message sent. Thank you!'
+      redirect_to new_contact_path
+    else
+      flash[:danger] = 'Sorry, your message has not been sent. Please make sure you are completing all required fields.'
+      redirect_to new_contact_path
+    end
   end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 
 end
