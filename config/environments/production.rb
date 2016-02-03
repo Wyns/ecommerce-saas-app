d982e22264d8eac5d18dc31bc14f853037cfddc8
jane3wallace <jane3wallace@gmail.com>
2016-02-03 04:15:55 +0000

    modified aws s3 setting for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
   # Amazon SW3 setting for image storage
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_host_name => 's3-website-us-east-1.amazonaws.com',
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
